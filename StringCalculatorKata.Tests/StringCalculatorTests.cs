581471b5e576f39736b7ed7a513b4df39ce7b680
Brandon Griffin <bgriffin@qualtrax.com>
2014-01-07 14:33:03 -0500

    First commit with a failing test.

---
+++ StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void EmptyStringReturns0()
+        {
+            var calculator = new StringCalculator();
+            
+            var actual = calculator.Calculate("");
+
+            Assert.That(actual, Is.EqualTo(0));
+        }
+    }
+}
---
+++ StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public object Calculate(string p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
